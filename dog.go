eabb501123b21bb66db8677fe08cf1c8aa722330
Gaurav Mangat <Gaurav.Mangat@watchguard.com>
2024-07-20 19:53:21 +0530

    Newest

--- dog.go
+++ dog.go
@@ -1,7 +1,7 @@
 package dog
+
 import "strings"
 
-
-func WhenGrowUp(s string) string{
-	return "When the puppy grows up it says: " + strings.ToUpper(s)
+func WhenGrowUp(s string) string {
+	return "When the puppy grows up it will say: " + strings.ToUpper(s)
 }
